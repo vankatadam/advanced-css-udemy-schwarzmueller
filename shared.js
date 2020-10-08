ec5e22662a17c29dd74919e0a2aa2e15e4d96267
vankatadam <vankatadam@gmail.com>
2020-10-08 16:04:37 +0200

    JS and CSS start

---
+++ shared.js
@@ -0,0 +1,5 @@
+var backdrop = document.querySelector(".backdrop");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+// console.dir(backdrop);
+
+backdrop.style.display = "block";
