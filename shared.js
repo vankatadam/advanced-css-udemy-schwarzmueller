beaf374effa4a127adabdea42d9622da3b3abc34
vankatadam <vankatadam@gmail.com>
2020-10-09 20:11:55 +0200

    burger menu

--- shared.js
+++ shared.js
@@ -1,5 +1,32 @@
 var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var noButton = document.querySelector("#modal__noButton");
+var burgerMenu = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 // console.dir(backdrop);
+// console.dir(selectPlanButtons);
+console.dir(noButton);
 
-backdrop.style.display = "block";
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+noButton.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
+
+function closeModal() {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+}
+
+burgerMenu.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
